79ec5ebf0b4f0eaf8781533006dd36aec3a834eb
vagrant <vagrant@precise32.(none)>
2015-07-02 00:04:38 +0000

    2.2.3 Improving the versioning

--- Gemfile
+++ Gemfile
@@ -34,6 +34,13 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'active_model_serializers', '0.9.3'
 
+group	:test	do
+  gem	"rspec-rails"
+  gem	"factory_girl_rails"
+  gem	'ffaker'
+end
+
+
 group :development, :test do
   # Use sqlite3 as the database for Active Record
   gem 'sqlite3'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :api,defaults:{format: :json} do
+    #will refer to controllers/api folder
+    scope module: :v1 do
+      # list the resources here
+    end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

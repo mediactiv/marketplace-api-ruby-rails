5f5d23c4c28cd4126e226e8fad1eb2d09f6af3b3
mparaiso <aikah@free.fr>
2015-07-06 00:19:48 +0200

    10.1.1 Products pagination

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -8,58 +8,73 @@ class Api::V1::ProductsController < ApplicationController
 
   # index lists all products
   def index
+    per_page = params[:per_page] || 10
     products = params[:product_ids].present? ?  get_product_list.where(id:params[:product_ids]) : get_product_list
-    products = products.page(params[:page]).per(params[:per_page])
+    products = products.page(params[:page]).per(per_page)
     render json: products,meta: {pagination:
-                                {per_page:params[:per_page],
-                                  total_pages:products.total_pages,
-                                  total_objects:products.total_count }}
-  end
-
-  # show shows a product
-  def show
-    respond_with Product.find(params[:id])
-  end
-
-  # create creates a new product
-  def create
-    product = current_user.products.build(product_params)
-    if product.save
-      #we use render because we want to return a 201 status code
-      render json: product, status: 201, location: [:api, product]
-    else
-      render json: { errors: product.errors }, status: 422
+      {per_page:per_page,
+        next_page: next_product_url(products),
+        previous_page: previous_product_url(products),
+        total_pages:products.total_pages,
+      total_objects:products.total_count }}
     end
-  end
 
-  def update
-    product = current_user.products.find(params[:id])
-    if product.update(product_params)
-      render json: product,status:200,location:[:api,product]
-    else
-      render json: {errors:product.errors},status:422
+    # show shows a product
+    def show
+      respond_with Product.find(params[:id])
     end
-  end
 
-  def destroy
-    product = current_user.products.find(params[:id])
-    product.destroy
-    head 204
-  end
+    # create creates a new product
+    def create
+      product = current_user.products.build(product_params)
+      if product.save
+        #we use render because we want to return a 201 status code
+        render json: product, status: 201, location: [:api, product]
+      else
+        render json: { errors: product.errors }, status: 422
+      end
+    end
 
-  private
+    def update
+      product = current_user.products.find(params[:id])
+      if product.update(product_params)
+        render json: product,status:200,location:[:api,product]
+      else
+        render json: {errors:product.errors},status:422
+      end
+    end
 
-  def product_params
-    params.require(:product).permit(:title, :price, :published)
-  end
+    def destroy
+      product = current_user.products.find(params[:id])
+      product.destroy
+      head 204
+    end
 
-  # get_product_list gets products depending on wether a user_id paramter 
-  # is present and whether
-  def get_product_list
-    if params[:user_id] && user = User.find(params[:user_id])
+    private
+
+    # next_product_url generates a url from a paginated list of products
+    # <ActiveRecord::Relation[<Product>]> => string
+    def next_product_url(paginated_products)
+      url_for controller: 'products' ,action: 'index',page: paginated_products.next_page
+    end
+
+    # previous_product_url generates a url from a paginated list of products
+    # <ActiveRecord::Relation[<Product>]> => string
+    def previous_product_url(paginated_products)
+      url_for controller: 'products' ,action: 'index',page: paginated_products.prev_page
+    end
+
+    def product_params
+      params.require(:product).permit(:title, :price, :published)
+    end
+
+    # get_product_list gets products depending on wether a user_id paramter
+    # is present and whether
+    def get_product_list
+      if params[:user_id] && user = User.find(params[:user_id])
         user.products.all
-    else
-      Product.all #kaminari needs a scope
+      else
+        Product.all #kaminari needs a scope
+      end
     end
   end
-end
